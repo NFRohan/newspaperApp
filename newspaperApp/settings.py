03305821f70d9a87033ee12c0128a3f1795c168e
NFRohan <nfrohan110@gmail.com>
2025-02-24 11:05:53 +0600

    redo urls, add bootstrap styling

--- newspaperApp/settings.py
+++ newspaperApp/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local
     'accounts',
+    'pages',
+
+    #3rd Party
+    'crispy_forms',
+    'crispy_bootstrap5',
 
 ]
 
@@ -128,3 +135,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
